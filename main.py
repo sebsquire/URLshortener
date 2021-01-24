50f9d1949a5f32675afc691fd15a2635eb9a988f
sebsquire <44575466+sebsquire@users.noreply.github.com>
2021-01-24 17:03:17 +0000

    urls into flask app now. need to fix shorten function which calls tinyurl api

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ Outputs: Shortened URL that redirects to correct site when called
 Program flow:
 
 '''
-from flask import Flask
+from flask import Flask, request
+import requests, contextlib
+from urllib.request import urlopen
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
 
 app = Flask(__name__)                           # instantiate Flask application
+
+
+def shorten_url(long_url):
+    '''
+    purpose: function that shortens urls
+    inputs: long url
+    outputs: short url that redirects to long url
+    '''
+    request_url = ('http://tinyurl.com/api-create.php?' +
+    urlencode({'url':long_url}))
+    with contextlib.closing(urlopen(request_url)) as response:
+        return response.read().decode('utf-8')
 
 
 @app.route('/health-check', methods=['GET'])
@@ -17,8 +36,10 @@ def healthCheck():
 
 @app.route('/shorten', methods=['POST'])
 def shorten():
-    return "OK shortened"
-    # todo make this function
+    incoming_url = request.form['url']
+    u = shorten_url(incoming_url)
+    #print(incoming_url)
+    return u
 
 
 if __name__ == "__main__":
